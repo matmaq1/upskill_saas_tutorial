c971590536845e32ec9a5af8b4b8d6e3a39e8f69
Mateusz Makowski <matmaq1@gmail.com>
2018-11-11 23:56:03 +0000

    added saving functionality to contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,18 @@
 class ContactsController < ApplicationController
     def new
+        @contact = Contact.new # blank object
     end
+    def create # saving
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to new_contact_path, notice: "Message sent."
+        else
+            redirect_to new_contact_path, notice: "Error"
+        end
+            
+    end
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
