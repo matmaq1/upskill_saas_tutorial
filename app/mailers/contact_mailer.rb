4dd5052da81b14b773cb9965f3ff50bcb9789b32
Mateusz Makowski <matmaq1@gmail.com>
2018-11-16 23:13:11 +0000

    cleaned up contact routes and flash messages

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+  default to:'matmaq1@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from @email, subject:'Test')
+  end
+    
+end
