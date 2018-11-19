3550bb65aaa2ce486340e6b315da01fb48b8aa07
Mateusz Makowski <matmaq1@gmail.com>
2018-11-19 05:49:37 +0000

    adding mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,10 @@ class ContactsController < ApplicationController
     def create # saving
         @contact = Contact.new(contact_params) #succed from the form already here
         if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = "Message sent correctly."
             redirect_to new_contact_path
         else
