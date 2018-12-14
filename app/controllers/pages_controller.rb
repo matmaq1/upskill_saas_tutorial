55ea28dfe7d758c3afeba3d233bb12570e2860b7
Mateusz Makowski <matmaq1@gmail.com>
2018-12-14 23:18:06 +0000

    added papremeters to signup plan on the gome page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
+    # GET /
     def home
+        @basic_plan = Plan.find(1)
+        @Pro_plan = Plan.find(2)
     end
+    
     def about
     end
 end
